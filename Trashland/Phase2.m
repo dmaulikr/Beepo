567d84b61841729cd3599f2cab9d416b0773f5fe
JJordani <tbj.cin@gmail.com>
2015-06-17 15:37:27 -0300

    Arvores Parque

--- Trashland/Phase2.m
+++ Trashland/Phase2.m
@@ -18,21 +18,65 @@
 
 #pragma mark - Tree Buttons
 - (IBAction)didClickTree1:(UIButton *)sender {
-    _arvore1.image = [UIImage imageNamed:@"parque-03"];
-    float xDiff = (888/2 - _arvore1.frame.size.width);
-    float yDiff = (1070/2 - _arvore1.frame.size.height);
-    NSLog(@"Original x: %f y: %f wid: %f hei: %f difX: %f difY: %f",_arvore1.frame.origin.x,_arvore1.frame.origin.y,_arvore1.frame.size.width,_arvore1.frame.size.height,xDiff,yDiff);
-    _arvore1.frame = CGRectMake(_arvore1.frame.origin.x - xDiff, _arvore1.frame.origin.y - yDiff, 888/2, 1070/2);
     
+    _arvore1.image = [UIImage imageNamed:@"parque-03"];
+    float yInicial = _arvore1.frame.origin.y;
+    float hInicial = _arvore1.frame.size.height;
+    float yFinal = yInicial - (1070/2 - hInicial);
+    float xInicial = _arvore1.frame.origin.x;
+    float wInicial = _arvore1.frame.size.width;
+    float xFinal = xInicial - (888/2 - wInicial)/2;
+    _arvore1.frame = CGRectMake(xFinal-9, yFinal, 888/2
+                                , 1070/2);
     sender.userInteractionEnabled = NO;
     
 }
 - (IBAction)didClickTree2:(UIButton *)sender {
+    _arvore2.image = [UIImage imageNamed:@"parque-04"];
+    float yInicial = _arvore2.frame.origin.y;
+    float hInicial = _arvore2.frame.size.height;
+    float yFinal = yInicial - (1298/2 - hInicial);
+    float xInicial = _arvore2.frame.origin.x;
+    float wInicial = _arvore2.frame.size.width;
+    float xFinal = xInicial - (1064/2 - wInicial)/2;
+    _arvore2.frame = CGRectMake(xFinal-9, yFinal, 1064/2
+                                , 1298/2);
+    sender.userInteractionEnabled = NO;
 }
 - (IBAction)didClickTree3:(UIButton *)sender {
+    _arvore3.image = [UIImage imageNamed:@"parque-05"];
+    float yInicial = _arvore3.frame.origin.y;
+    float hInicial = _arvore3.frame.size.height;
+    float yFinal = yInicial - (1300/2 - hInicial);
+    float xInicial = _arvore3.frame.origin.x;
+    float wInicial = _arvore3.frame.size.width;
+    float xFinal = xInicial - (1080/2 - wInicial)/2;
+    _arvore3.frame = CGRectMake(xFinal-11, yFinal, 1080/2
+                                , 1300/2);
+    sender.userInteractionEnabled = NO;
 }
 - (IBAction)didClickTree4:(UIButton *)sender {
+    _arvore4.image = [UIImage imageNamed:@"parque-06"];
+    float yInicial = _arvore4.frame.origin.y;
+    float hInicial = _arvore4.frame.size.height;
+    float yFinal = yInicial - (1068/2 - hInicial);
+    float xInicial = _arvore4.frame.origin.x;
+    float wInicial = _arvore4.frame.size.width;
+    float xFinal = xInicial - (874/2 - wInicial)/2;
+    _arvore4.frame = CGRectMake(xFinal-8, yFinal, 874/2
+                                , 1068/2);
+    sender.userInteractionEnabled = NO;
 }
 - (IBAction)didClickTree5:(UIButton *)sender {
+    _arvore5.image = [UIImage imageNamed:@"parque-07"];
+    float yInicial = _arvore5.frame.origin.y;
+    float hInicial = _arvore5.frame.size.height;
+    float yFinal = yInicial - (1298/2 - hInicial);
+    float xInicial = _arvore5.frame.origin.x;
+    float wInicial = _arvore5.frame.size.width;
+    float xFinal = xInicial - (1064/2 - wInicial)/2;
+    _arvore5.frame = CGRectMake(xFinal-9, yFinal, 1064/2
+                                , 1298/2);
+    sender.userInteractionEnabled = NO;
 }
 @end
