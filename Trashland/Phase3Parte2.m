f2d2327e857fdc3f2c7c6e2d2e53d24631323648
Bruno Muniz Azevedo Filho <bf2002@gmail.com>
2015-06-20 11:52:36 -0300

    restando prefeitura

--- Trashland/Phase3Parte2.m
+++ Trashland/Phase3Parte2.m
@@ -21,23 +21,37 @@
     
     _viewCarro1.podeY = YES;
     _viewCarro2.podeY = YES;
+    
     [self checkWayFree];
 }
 
 -(void)checkWayFree{
-    if (true) {
-      //ANIMACAO DA PASSAGEM DA AMBULANCIA
-        //and then...
+    if (self.viewCarro1.center.y < 260) {
+        self.viewCarro1.podeY = false;
+    }
+    if (self.viewCarro2.center.y > 540) {
+        self.viewCarro2.podeY = false;
+    }
+    if (self.viewCarro1.center.y < 260 && self.viewCarro2.center.y > 540) {
         
-        PhasesChoose *game = [self.storyboard instantiateViewControllerWithIdentifier:@"PhasesChooseVC"];
-        [game setModalPresentationStyle:UIModalPresentationFullScreen];
-        self.player.medalha2fase3 = true;
-        game.player = self.player;
-        [self presentViewController:game animated:NO completion:nil];
+         [NSTimer scheduledTimerWithTimeInterval:2.0 target:self selector:@selector(escolhaDeFases) userInfo:nil repeats:NO];
+        
+        [UIView animateWithDuration:4 delay:0.0 options:UIViewAnimationCurveEaseIn animations:^{
+                self.ambulancia.frame = CGRectMake(1200, self.ambulancia.frame.origin.y, self.ambulancia.frame.size.width, self.ambulancia.frame.size.height); } completion:nil];
     }
     else{
-        [NSTimer scheduledTimerWithTimeInterval:0.3f target:self selector:@selector(checkWayFree) userInfo:nil repeats:NO];
+        [NSTimer scheduledTimerWithTimeInterval:0.1f target:self selector:@selector(checkWayFree) userInfo:nil repeats:NO];
     }
+}
+
+
+-(void)escolhaDeFases{
+                PhasesChoose *game = [self.storyboard instantiateViewControllerWithIdentifier:@"PhasesChooseVC"];
+            [game setModalPresentationStyle:UIModalPresentationFullScreen];
+            self.player.medalha2fase3 = true;
+    
+            game.player = self.player;
+            [self presentViewController:game animated:YES completion:nil];
 }
 
 @end
