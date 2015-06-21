57e03e589f512607f5970f8b4a0a1b7d6b99652f
Bruno Muniz Azevedo Filho <bf2002@gmail.com>
2015-06-21 15:37:38 -0300

    add files

--- Trashland/Puzzle4.h
+++ Trashland/Puzzle4.h
@@ -1,17 +1,17 @@
 //
-//  Story4.h
+//  Puzzle4.h
 //  Trashland
 //
-//  Created by Bruno Muniz Azevedo Filho on 6/19/15.
+//  Created by Bruno Muniz Azevedo Filho on 6/21/15.
 //  Copyright (c) 2015 Thiago Borges Jordani. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import "Player.h"
 
-@interface Story4 : UIViewController
+@interface Puzzle4 : UIViewController
 
-@property (nonatomic) IBOutlet UIImageView* fundo;
 
 @property (nonatomic) Player* player;
+
 @end
